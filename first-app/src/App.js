028422017af8bff94838139716a0228c850c026b
AtulLahade <atul.lahade@mendix.com>
2023-12-07 13:16:33 +0530

    react fist app

---
+++ first-app/src/App.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState('');
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json();
+    console.log(data.slip.advice);
+    setAdvice(data.slip.advice);
+    setCount(count + 1);
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
+      <Message count={count} />
+    </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>
+      You have read <strong>{props.count}</strong> pieces of advice
+    </p>
+  );
+}
+
